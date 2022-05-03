9fb38fb709e24169772f64dae67018f5c3a3fb09
Michael Kendrick <michael@locumsmart.net>
2022-05-03 17:32:04 -0500

    added voter data

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require("express");
-const mysql = require("mysql2");
+
 const inputCheck = require("./utils/inputCheck");
+const db = require("./db/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "election",
-  },
-  console.log("Connected to the election database.")
-);
 
 app.get("/api/candidates", (req, res) => {
   const sql = `SELECT candidates.*, parties.name AS party_name FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id`;
